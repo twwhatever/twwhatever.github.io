763b9e1daf31db8de8dfdc872976a8ff30e01863
Ted Wild <wildted@live.com>
2019-02-18 03:19:08 -0800

    Update pelican configuration

---
+++ pelicanconf.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Ted'
+SITENAME = "Ted Wild's Blog"
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('My old page', 'http://pages.cs.wisc.edu/~wildt/'),)
+
+# Social widget
+SOCIAL = (('LinkedIn', 'https://www.linkedin.com/in/ted-wild-b0b458175'),
+          ('GitHub', 'https://github.com/twwhatever'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
